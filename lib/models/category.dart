9a4fa18c2cf0ac46f30a8e75e174d39abeef3057
Joel Marpaung <myitlife.jm@gmail.com>
2022-11-07 16:34:33 +0700

    updating model for json

---
+++ lib/models/category.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+class Category {
+  Category({
+    required this.name,
+  });
+
+  final String name;
+
+  factory Category.fromJson(Map<String, dynamic> json) => Category(
+    name: json["name"],
+  );
+
+}
+
+List<Category> parseCategories(String? json) {
+  if (json == null) {
+    return [];
+  }
+
+  final List parsed = jsonDecode(json);
+  return parsed.map((json) => Category.fromJson(json)).toList();
+}
